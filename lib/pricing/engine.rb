935d8f293dbf9c0ce68565292a9241812f3c3091
thanakorn.b <thang@odds.team>
2025-08-30 16:44:10 +0700

    config

--- lib/pricing/engine.rb
+++ lib/pricing/engine.rb
@@ -3,15 +3,15 @@ module Pricing
     isolate_namespace Pricing
   end
 
-  initializer "pricing.notifications" do |app|
-      app.config.to_prepare do
-        Engine.root.join("app/listeners").tap do |listeners|
-          Dir.glob("#{listeners}/**/*_listener.rb").sort.each do |listener|
-            p "loading listener #{listener}"
-            load listener
-          end
-        end
-      end
-    end
+  # initializer "pricing.notifications" do |app|
+  #     app.config.to_prepare do
+  #       Engine.root.join("app/listeners").tap do |listeners|
+  #         Dir.glob("#{listeners}/**/*_listener.rb").sort.each do |listener|
+  #           p "loading listener #{listener}"
+  #           load listener
+  #         end
+  #       end
+  #     end
+  #   end
 end
 
